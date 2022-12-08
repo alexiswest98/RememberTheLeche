758f857d1b7106ab31167e5bbae17ddf15eb68f7
Alexis West <alexishwest98@gmail.com>
2022-12-07 16:26:35 -0800

    Added models, fixed branches

--- ignore.py
+++ ignore.py
@@ -1,2 +1,96 @@
 def ignore():
     pass
+
+from flask_sqlalchemy import SQLAlchemy 
+
+db = SQLAlchemy()
+
+# join table for members
+member = db.Table(
+    'members',
+    db.Model.metadata,
+    db.Column('user_id', db.Integer, db.ForeignKey('users.id'), primary_key=True, nullable=False),
+    db.Column('group_id', db.Integer, db.ForeignKey('groups.id'), primary_key=True, nullable=False )
+)
+
+
+# Join table for followers
+follows = db.Table(
+    "follows", 
+    db.Column("follower_id", db.Integer, db.ForeignKey("users.id")),
+    db.Column("followed_id", db.Integer, db.ForeignKey("users.id"))
+)
+
+
+class Task(db.Model):
+    __tablename__ = 'tasks'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    list = db.Column(db.Boolean, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    due = db.Column(db.Date, nullable=False)
+    completed = db.Column(db.Boolean, default=False)
+    completed_by = db.Column(db.Integer, db.ForeignKey('users.id'))
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now())
+    # Relationships
+    user_who_completed_task = db.Relationship('User', back_populates='user_completed_task')
+    subtasks = db.relationship('SubTask', back_populates='task')
+    task_creator = db.relationship('User', back_populates='user_who_created_task')
+    task_to_group = db.relationship('Group', back_populates='group_to_task')
+
+class SubTask(db.Model):
+    __tablename__ = 'subtasks'
+
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(500), nullable=False)
+    task_id = db.Column(db.Integer, db.ForeignKey('tasks.id'), nullable=False)
+    due = db.Column(db.Date, nullable=False)
+    completed = db.Column(db.Boolean, default=False)
+    completed_by = db.Column(db.Integer, db.ForeignKey('users.id'))
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now())
+    # Relationships
+    user_who_completed_subtask = db.relationship('User', back_populates='user_completed_subtask')
+    task = db.relationship('Task', back_populates='subtasks')
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), nullable=False)
+    password = db.Column(db.String(500), nullable=False)
+    email = db.Column(db.String(100), nullable=False)
+    image_url = db.Column(db.String(1000))
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now())
+    # Relationships
+    user_completed_task = db.relationship('Task', back_populates='user_who_completed_task')
+    user_who_created_task = db.relationship('Task', back_populates='task_creator')
+    user_who_created_subtask = db.relationship('SubTask', back_populates='user_who_completed_subtask')
+    user_who_created_group = db.relationship('Group', back_populates='group_to_user')
+
+    followers = db.relationship(
+        "User", 
+        secondary=follows,
+        primaryjoin=(follows.c.follower_id == id),
+        secondaryjoin=(follows.c.followed_id == id),
+        backref=db.backref("following", lazy="dynamic"),
+        lazy="dynamic"
+        )
+
+
+
+class Groups(db.Model):
+    __tablename__ = 'groups'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.VARCHAR(50), nullable=False)
+    image_url = db.Column(db.VARCHAR(100), default='https://moodlehub.ca/pluginfile.php/6842/mod_book/chapter/9131/group2.jpg')
+    owner_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now())
+    # Relationships
+    group_to_task = db.relationship('Task', back_populates='task_to_group') 
+    group_to_user = db.relationshiop('User', back_populates='user_who_created_group')
