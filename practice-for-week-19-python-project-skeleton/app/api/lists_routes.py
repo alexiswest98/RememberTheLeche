81c7749a50bc72952935a06f60eb43064faf7bfd
Gabriel <gabetd0904@gmail.com>
2022-12-09 18:00:36 -0600

    created folders for routes

--- practice-for-week-19-python-project-skeleton/app/api/lists_routes.py
+++ practice-for-week-19-python-project-skeleton/app/api/lists_routes.py
@@ -1,2 +1,8 @@
+from flask import Blueprint, jsonify
+from flask_login import login_required
+from app.models import List
+
+user_routes = Blueprint('lists', __name__)
+
 def lists():
   pass
--- practice-for-week-19-python-project-skeleton/app/api/tasks_routes.py
+++ practice-for-week-19-python-project-skeleton/app/api/tasks_routes.py
@@ -1,2 +1,7 @@
+from flask import Blueprint, jsonify
+from flask_login import login_required
+from app.models import Task
+
+user_routes = Blueprint('tasks', __name__)
 def tasks():
   pass
