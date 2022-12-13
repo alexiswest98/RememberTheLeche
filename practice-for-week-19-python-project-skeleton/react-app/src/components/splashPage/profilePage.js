5134b684d5b932206d2b97838693466747e2b0ac
Gabriel <gabetd0904@gmail.com>
2022-12-13 16:05:12 -0600

    evan now gets to enjoy this pain

--- practice-for-week-19-python-project-skeleton/react-app/src/components/splashPage/profilePage.js
+++ practice-for-week-19-python-project-skeleton/react-app/src/components/splashPage/profilePage.js
@@ -7,17 +7,24 @@ import { GetAllListsThunk } from "../../store/lists";
 const ProfileForm = () => {
   const user = useSelector(state => state.session.user);
   const dispatch = useDispatch();
-  const lists = useSelector(state => state.session.lists)
-  const onload = async() => {
-    await dispatch(GetAllListsThunk());
-  }
-  onload()
-  console.log("lists= ",lists)
-  console.log(user)
+  const lists = useSelector(state => state.lists)
+
+
+  useEffect(() => {
+    dispatch(GetAllListsThunk())
+  }, [dispatch])
+  console.log('Object values for list = ',Object.values(lists))
+
   return(
     <div>
       <p>{user?.username}</p>
       <img src={user?.image_url}/>
+      {Object.values(lists).map(list => (
+        <div key={list.id}>
+          {list.name}
+        </div>
+      ))}
+      <div>follows components</div>
     </div>
   )
 }
